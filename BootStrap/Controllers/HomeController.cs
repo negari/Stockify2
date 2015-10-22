84e11ec7c75116618809c7ee6d7a4a9d13aadbd6
NEGAR <negar.gh@gmail.com>
2015-10-22 22:58:02 +1000

    first commit

---
+++ BootStrap/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            int[] array = new int[] { 3, 1, 4, 5, 2 };
+            Array.Sort<int>(array,
+                            new Comparison<int>(
+                                    (i1, i2) => i2.CompareTo(i1)
+                            ));
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
